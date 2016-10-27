8fc950324fc57a211bcc2b68ecf9f6f68020b392
John Mertens <john.mertens@gmail.com>
2016-10-27 00:18:01 -0700

    Add eslint; convert to es6

--- index.js
+++ index.js
@@ -1,57 +1,54 @@
 #!/usr/bin/env node
-// Modules
-var fs = require('graceful-fs'),
-    gm = require('gm'),
-    _  = require('lodash'),
-    async = require('async'),
-    ImageRecord = require('./lib/image_record');
+const fs = require('graceful-fs');
+const gm = require('gm');
+const _ = require('lodash');
+const async = require('async');
+const ImageRecord = require('./lib/image_record');
 
 // Sub modules
-var imageMagick = gm.subClass({ imageMagick: true }),
-    ExifImage  = require('exif').ExifImage;
+const imageMagick = gm.subClass({ imageMagick: true });
+const ExifImage = require('exif').ExifImage;
 
 // Globals
-var originalsPath = "/Users/mertonium/Pictures/test",
-    exportsPath = "/Users/mertonium/Pictures/test/exports",
-    s3Path = "https://s3.amazonaws.com/mertonium_public/zamar";
-    records = [];
+const originalsPath = '/Users/mertonium/Pictures/test';
+const exportsPath = '/Users/mertonium/Pictures/test/exports';
+const s3Path = 'https://s3.amazonaws.com/mertonium_public/zamar';
+const records = [];
 
 // Randos
-var originalsArr = fs.readdirSync(originalsPath),
-    originals = _.filter(originalsArr, function(x) {
-      return x.search(/\.jpg$/i) > -1;
-    });
+const originalsArr = fs.readdirSync(originalsPath);
+const originals = _.filter(originalsArr, x => (x.search(/\.jpg$/i) > -1));
 
-var processFile = function(filename, done) {
-  var record = new ImageRecord(filename, originalsPath, exportsPath, s3Path);
+function processFile(filename, done) {
+  const record = new ImageRecord(filename, originalsPath, exportsPath, s3Path);
 
   async.waterfall([
-    function(cb) {
+    (cb) => {
+      /* eslint no-new: "off" */
       new ExifImage({ image: record.originalUri }, cb);
     },
-    function(data, cb) {
+    (data, cb) => {
       record.addExifData(data);
 
-      if(record.longitude != null && record.latitude != null) {
+      if (record.longitude != null && record.latitude != null) {
         records.push(record);
-        // Create new image...
         imageMagick(record.originalUri).resize(400, 400).noProfile().write(record.exportUri, cb);
       } else {
         cb();
       }
-    }
+    },
   ], done);
-};
+}
 
-async.eachLimit(originals, 5, processFile, function(err) {
-  if(err) console.error(err.message);
+async.eachLimit(originals, 5, processFile, (err) => {
+  if (err) console.error(err.message);
 
-  var featureCollection = _.map(records, function(r) { return r.asDocument(); });
-  var geojson = {
+  const featureCollection = _.map(records, r => r.asDocument());
+  const geojson = {
     type: 'FeatureCollection',
-    features: featureCollection
+    features: featureCollection,
   };
 
   console.log(JSON.stringify(geojson, null, 2));
-  console.log("Processed " + featureCollection.length + " records");
+  console.log(`Processed ${featureCollection.length} records`);
 });
