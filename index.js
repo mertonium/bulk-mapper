89f5bf917e6e47738bd9fdcb5529d9bb897f8c65
John Mertens <john.mertens@gmail.com>
2016-10-26 23:38:07 -0700

    Make npm module; extract ImageRecord into a module; remove couch stuff

--- index.js
+++ index.js
@@ -1,114 +1,29 @@
+#!/usr/bin/env node
 // Modules
 var fs = require('graceful-fs'),
     gm = require('gm'),
-    _  = require('underscore'),
+    _  = require('lodash'),
     async = require('async'),
-    sha1 = require('sha1'),
-    cradle = require('cradle');
+    ImageRecord = require('./lib/image_record');
 
 // Sub modules
 var imageMagick = gm.subClass({ imageMagick: true }),
     ExifImage  = require('exif').ExifImage;
 
 // Globals
-var originalsPath = "/Users/mertonium/Pictures/zamar",
-    exportsPath = "/Users/mertonium/Pictures/zamar/exports",
+var originalsPath = "/Users/mertonium/Pictures/test",
+    exportsPath = "/Users/mertonium/Pictures/test/exports",
     s3Path = "https://s3.amazonaws.com/mertonium_public/zamar";
     records = [];
 
-// Database
-var db = new(cradle.Connection)('http://admin:admin@127.0.0.1', 5984, {
-           cache: true,
-           raw: false,
-           forceSave: true
-         }).database('zamar');
-
-
 // Randos
 var originalsArr = fs.readdirSync(originalsPath),
-    originals = _.select(originalsArr, function(x) {
+    originals = _.filter(originalsArr, function(x) {
       return x.search(/\.jpg$/i) > -1;
     });
 
-// Class for the [poorly named] image records.
-function ImageRecord(filename) {
-  this.filename = filename;
-  this._id = sha1(this.filename);
-
-  this.originalUri = originalsPath + '/' + this.filename;
-  this.exportUri = exportsPath + '/' + this.filename.replace(' ', '_');
-  this.s3Url = s3Path + '/' + this.filename.replace(' ', '_');
-
-  this.addExifData = function(exifData) {
-    var lat = exifData.gps.GPSLatitude,
-        lng = exifData.gps.GPSLongitude,
-        latRef = exifData.gps.GPSLatitudeRef,
-        lngRef = exifData.gps.GPSLongitudeRef;
-
-    this.created_at = exifData.exif.CreateDate;
-    this.latitude = this.convertToDecimal(lat, latRef);
-    this.longitude = this.convertToDecimal(lng, lngRef);
-  };
-
-  this.convertToDecimal = function(gpsArr, gpsRef) {
-    var decimalPiece, baseResult;
-
-    if(!gpsArr || gpsArr.length != 3) return;
-
-    decimalPiece = (gpsArr[1] * 60 + gpsArr[2]) / 3600;
-    baseResult = gpsArr[0] + decimalPiece;
-    return (gpsRef == 'S' || gpsRef == 'W') ? -1 * baseResult : baseResult;
-  };
-
-  this.asObject = function() {
-    return {
-      filename : this.filename,
-      created_at : this.created_at,
-      latitude : this.latitude,
-      longitude : this.longitude
-    };
-  };
-
-  this.asDocument = function() {
-    return {
-      id: this._id,
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [this.longitude, this.latitude]
-      },
-      properties : {
-        title: '',
-        filename : this.filename,
-        created_at : this.created_at,
-        photo_url : this.s3Url
-      }
-    };
-  };
-
-  this.asPublicArtFinder = function() {
-    return {
-      id: this._id,
-      title: '',
-      artist: 'zamar',
-      description: '',
-      discipline: '',
-      location_description: '',
-      full_address: '',
-      geometry: {
-        type: "Point",
-        coordinates: [this.longitude, this.latitude]
-      },
-      image_urls: [this.s3Url],
-      data_source: 'mertonium',
-      doc_type: 'artwork',
-      created_at : this.created_at
-    };
-  };
-}
-
 var processFile = function(filename, done) {
-  var record = new ImageRecord(filename);
+  var record = new ImageRecord(filename, originalsPath, exportsPath, s3Path);
 
   async.waterfall([
     function(cb) {
@@ -138,4 +53,5 @@ async.eachLimit(originals, 5, processFile, function(err) {
   };
 
   console.log(JSON.stringify(geojson, null, 2));
+  console.log("Processed " + featureCollection.length + " records");
 });
